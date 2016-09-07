b71c7f4d96b9aa08fc31a728d9fb24f87d265baf
sonorangirl <manahan.erin@gmail.com>
2016-09-07 12:45:58 -0700

    Added return key functionality

--- js/tasks.js
+++ js/tasks.js
@@ -1,35 +1,30 @@
 $(document).ready(function() {
 
 	//Add tasks to list
-	$('#add-task').click(function() {
-    
+	function getTask() {
 		var addTask = $('input[name=taskItem]').val();
     
 		event.preventDefault();
 		$('.task-list').append('<li class="item">' + addTask + '</li>');
 		$('.task-options').append('<div class="btn-group pull-right" role="group" aria-label="task-options"><button type="button" class="btn btn-default select">select</button><button type="button" class="btn btn-default completed">completed</button><button type="button" class="btn btn-default delete">delete</button></div>');
+		
 		//set input field back to nothing
 		$('input[name=taskItem]').val("");
+	}
+
+	$('#add-task').click(function() {
+		getTask();
+	});
+
+	$('#taskItemInput').keypress( function(key) {
+		if (key.which == 13) {
+			console.log('Add task by return key');
+			getTask();
+		}
 	});
 
 
-
-	//Task Buttons
-
-	//Get the task associated with its buttons - possible solution - create a variable to get the current index of the div for the buttons - $('.task-options').index(this)
-	//use jQuery eq(var) to select the li with the same index (they would be the same, since created at the same time
-
-
-	// var selectTaskItem = function(options) {
-
-	// 	var indexOfTask = $('.' + options).index(this);
-
-	// 	var selectTask = $('.task-list li').eq(indexOfTask);
-
-	// 	console.log('function working, index of task is ' + indexOfTask);
-	
-	// }
-
+	//// Task Buttons ////
 
 	//Select task and add to status
 	$(".task-options").on("click", ".select", function(){
